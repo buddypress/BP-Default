buddypress/BP-Default

commit 320633cbd235e2d1cadafe54b0f5396ca7a13aa8

Rework how the file is included to avoid a warning with the Theme Check
git-svn-id: https://buddypress.svn.wordpress.org/trunk@4180 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-themes/bp-default/activity/activity-loop.php
@@ -31,7 +31,7 @@
 
 	<?php while ( bp_activities() ) : bp_the_activity(); ?>
 
-		<?php include( locate_template( array( 'activity/entry.php' ), false ) ); ?>
+		<?php locate_template( array( 'activity/entry.php' ), true ); ?>
 
 	<?php endwhile; ?>
 

commit 14fdc48471920b2718e7db4f7fbb34c3f606b82c

Fix activity stream bug which I introduced in r4180
git-svn-id: https://buddypress.svn.wordpress.org/trunk@4193 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-themes/bp-default/activity/activity-loop.php
@@ -31,7 +31,7 @@
 
 	<?php while ( bp_activities() ) : bp_the_activity(); ?>
 
-		<?php locate_template( array( 'activity/entry.php' ), true ); ?>
+		<?php locate_template( array( 'activity/entry.php' ), true, false ); ?>
 
 	<?php endwhile; ?>
 