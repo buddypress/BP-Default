buddypress/BP-Default

commit ad7f2edc4dc2d61ca59846cd1a3837dbc39aedb9

Audit the usage of $bp->forums->slug and $bp->forums->root_slug. See #3325.
git-svn-id: https://buddypress.svn.wordpress.org/trunk@4652 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-core/bp-core-functions.php
@@ -831,7 +831,7 @@ function bp_core_action_search_site( $slug = '' ) {
 				break;
 
 			case 'forums':
-				$slug = bp_is_active( 'forums' ) ? $bp->forums->root_slug : '';
+				$slug = bp_is_active( 'forums' ) ? bp_get_groups_root_slug() : '';
 				$query_string = '/?fs=';
 				break;
 

bp-forums/bp-forums-filters.php
@@ -75,7 +75,7 @@ function bp_forums_filter_kses( $content ) {
 function bp_forums_filter_tag_link( $link, $tag, $page, $context ) {
 	global $bp;
 
-	return apply_filters( 'bp_forums_filter_tag_link', bp_get_root_domain() . '/' . $bp->forums->root_slug . '/tag/' . $tag . '/' );
+	return apply_filters( 'bp_forums_filter_tag_link', bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/tag/' . $tag . '/' );
 }
 add_filter( 'bb_get_tag_link', 'bp_forums_filter_tag_link', 10, 4);
 

bp-forums/bp-forums-template.php
@@ -688,7 +688,7 @@ function bp_my_forum_topics_link() {
 	function bp_get_my_forum_topics_link() {
 		global $bp;
 
-		return apply_filters( 'bp_get_my_forum_topics_link', bp_get_root_domain() . '/' . $bp->forums->slug . '/personal/' );
+		return apply_filters( 'bp_get_my_forum_topics_link', bp_get_root_domain() . '/' . bp_get_forums_root_slug() . '/personal/' );
 	}
 
 function bp_unreplied_forum_topics_link() {
@@ -697,7 +697,7 @@ function bp_unreplied_forum_topics_link() {
 	function bp_get_unreplied_forum_topics_link() {
 		global $bp;
 
-		return apply_filters( 'bp_get_unreplied_forum_topics_link', bp_get_root_domain() . '/' . $bp->forums->slug . '/unreplied/' );
+		return apply_filters( 'bp_get_unreplied_forum_topics_link', bp_get_root_domain() . '/' . bp_get_forums_root_slug() . '/unreplied/' );
 	}
 
 
@@ -707,7 +707,7 @@ function bp_popular_forum_topics_link() {
 	function bp_get_popular_forum_topics_link() {
 		global $bp;
 
-		return apply_filters( 'bp_get_popular_forum_topics_link', bp_get_root_domain() . '/' . $bp->forums->slug . '/popular/' );
+		return apply_filters( 'bp_get_popular_forum_topics_link', bp_get_root_domain() . '/' . bp_get_forums_root_slug() . '/popular/' );
 	}
 
 function bp_newest_forum_topics_link() {
@@ -716,7 +716,7 @@ function bp_newest_forum_topics_link() {
 	function bp_get_newest_forum_topics_link() {
 		global $bp;
 
-		return apply_filters( 'bp_get_newest_forum_topics_link', bp_get_root_domain() . '/' . $bp->forums->slug . '/' );
+		return apply_filters( 'bp_get_newest_forum_topics_link', bp_get_root_domain() . '/' . bp_get_forums_root_slug() . '/' );
 	}
 
 function bp_forum_topic_type() {
@@ -725,11 +725,10 @@ function bp_forum_topic_type() {
 	function bp_get_forum_topic_type() {
 		global $bp;
 
-		if ( !bp_is_directory() || !$bp->current_action )
+		if ( !bp_is_directory() || !bp_current_action() )
 			return 'newest';
 
-
-		return apply_filters( 'bp_get_forum_topic_type', $bp->current_action );
+		return apply_filters( 'bp_get_forum_topic_type', bp_current_action() );
 	}
 
 function bp_forums_tag_name() {
@@ -738,8 +737,12 @@ function bp_forums_tag_name() {
 	function bp_get_forums_tag_name() {
 		global $bp;
 
-		if ( bp_is_directory() && $bp->forums->slug == $bp->current_component )
-			return apply_filters( 'bp_get_forums_tag_name', $bp->action_variables[0] );
+		if ( bp_is_directory() && bp_is_forums_component() && !empty( $bp->action_variables[0] ) )
+			$tag_name = $bp->action_variables[0];
+		else
+			$tag_name = false;
+
+		return apply_filters( 'bp_get_forums_tag_name', $tag_name );
 	}
 
 function bp_forum_pagination() {
@@ -1176,7 +1179,7 @@ function bp_forum_directory_permalink() {
 	function bp_get_forum_directory_permalink() {
 		global $bp;
 
-		return apply_filters( 'bp_get_forum_directory_permalink', trailingslashit( bp_get_root_domain() . '/' . $bp->forums->root_slug ) );
+		return apply_filters( 'bp_get_forum_directory_permalink', trailingslashit( bp_get_root_domain() . '/' . bp_get_groups_root_slug() ) );
 	}
 
 function bp_forums_tag_heat_map( $args = '' ) {

commit 494c4c721aa4b2c88432ddcc8ff85c543f8f0168

Fix incorrect function name in bp_core_action_search_site() introduced in r4652.
git-svn-id: https://buddypress.svn.wordpress.org/trunk@5094 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-core/bp-core-functions.php
@@ -845,7 +845,7 @@ function bp_core_action_search_site( $slug = '' ) {
 				break;
 
 			case 'forums':
-				$slug = bp_is_active( 'forums' ) ? bp_get_groups_root_slug() : '';
+				$slug = bp_is_active( 'forums' ) ? bp_get_forums_root_slug() : '';
 				$query_string = '/?fs=';
 				break;
 