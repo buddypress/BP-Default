buddypress/BP-Default

commit fc4deac0bc3b9f8123924d22864b5204322d9f3f

Check to see whether a user is already a member of a group before creating a new membership in BP_Groups_Member::save(). Fixes #4113
git-svn-id: https://buddypress.svn.wordpress.org/trunk@5974 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-groups/bp-groups-classes.php
@@ -761,10 +761,16 @@ class BP_Groups_Member {
 
 		do_action_ref_array( 'groups_member_before_save', array( &$this ) );
 
-		if ( !empty( $this->id ) )
+		if ( !empty( $this->id ) ) {
 			$sql = $wpdb->prepare( "UPDATE {$bp->groups->table_name_members} SET inviter_id = %d, is_admin = %d, is_mod = %d, is_banned = %d, user_title = %s, date_modified = %s, is_confirmed = %d, comments = %s, invite_sent = %d WHERE id = %d", $this->inviter_id, $this->is_admin, $this->is_mod, $this->is_banned, $this->user_title, $this->date_modified, $this->is_confirmed, $this->comments, $this->invite_sent, $this->id );
-		else
+		} else {
+			// Ensure that user is not already a member of the group before inserting
+			if ( $wpdb->get_var( $wpdb->prepare( "SELECT id FROM {$bp->groups->table_name_members} WHERE user_id = %d AND group_id = %d LIMIT 1", $this->user_id, $this->group_id ) ) ) {
+				return false;
+			}
+			
 			$sql = $wpdb->prepare( "INSERT INTO {$bp->groups->table_name_members} ( user_id, group_id, inviter_id, is_admin, is_mod, is_banned, user_title, date_modified, is_confirmed, comments, invite_sent ) VALUES ( %d, %d, %d, %d, %d, %d, %s, %s, %d, %s, %d )", $this->user_id, $this->group_id, $this->inviter_id, $this->is_admin, $this->is_mod, $this->is_banned, $this->user_title, $this->date_modified, $this->is_confirmed, $this->comments, $this->invite_sent );
+		}
 
 		if ( !$wpdb->query( $sql ) )
 			return false;

commit 1bb6085d119832fbdb7d9a8d63e973a9605b153f

Ensure that already-invited members can also request membership to private groups
This fixes a regression from r5974, in which an overly greedy check was put in
place to avoid duplicate group memberships. The pre-save check now only bails
when a confirmed membership is found.

Fixes #4113

git-svn-id: https://buddypress.svn.wordpress.org/trunk@6276 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-groups/bp-groups-classes.php
@@ -819,7 +819,7 @@ class BP_Groups_Member {
 			$sql = $wpdb->prepare( "UPDATE {$bp->groups->table_name_members} SET inviter_id = %d, is_admin = %d, is_mod = %d, is_banned = %d, user_title = %s, date_modified = %s, is_confirmed = %d, comments = %s, invite_sent = %d WHERE id = %d", $this->inviter_id, $this->is_admin, $this->is_mod, $this->is_banned, $this->user_title, $this->date_modified, $this->is_confirmed, $this->comments, $this->invite_sent, $this->id );
 		} else {
 			// Ensure that user is not already a member of the group before inserting
-			if ( $wpdb->get_var( $wpdb->prepare( "SELECT id FROM {$bp->groups->table_name_members} WHERE user_id = %d AND group_id = %d LIMIT 1", $this->user_id, $this->group_id ) ) ) {
+			if ( $wpdb->get_var( $wpdb->prepare( "SELECT id FROM {$bp->groups->table_name_members} WHERE user_id = %d AND group_id = %d AND is_confirmed = 1 LIMIT 1", $this->user_id, $this->group_id ) ) ) {
 				return false;
 			}
 			