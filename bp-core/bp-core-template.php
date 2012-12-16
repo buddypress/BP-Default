buddypress/BP-Default

commit e9975693a9dbf96809318af8282294a60d5d896a

Revert r6587:
* Introduce bp_get_email_subject() function, to handle formatting of email subjects.
* For all components with notifications, including deprecated files.
* Fixes #4401, better this time.

git-svn-id: https://buddypress.svn.wordpress.org/trunk@6589 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-core/bp-core-template.php
@@ -539,6 +539,31 @@ function bp_registration_needs_activation() {
 }
 
 /**
+ * Retrieve a client friendly version of the root blog name, plus take care of
+ * the typical formatting bits and bobs.
+ *
+ * The blogname option is escaped with esc_html on the way into the database in
+ * sanitize_option, we want to reverse this for the plain text arena of emails.
+ *
+ * @link http://buddypress.trac.wordpress.org/ticket/4401
+ * @since BuddyPress (1.7)
+ * @return string
+ */
+function bp_get_email_subject( $args = array() ) {
+
+	$r = wp_parse_args( $args, array(
+		'before'  => '[',
+		'after'   => ']',
+		'default' => __( 'Community', 'buddypress' ),
+		'text'    => ''
+	) );
+
+	$subject = $r['before'] . wp_specialchars_decode( bp_get_option( 'blogname', $default ), ENT_QUOTES ) . $r['after'] . ' ' . $r['text'];
+
+	return apply_filters( 'bp_get_email_subject', $subject, $r );
+}
+
+/**
  * Allow templates to pass parameters directly into the template loops via AJAX
  *
  * For the most part this will be filtered in a theme's functions.php for example

bp-activity/bp-activity-notifications.php
@@ -70,8 +70,7 @@ function bp_activity_at_message_notification( $activity_id, $receiver_user_id ) 
 		// Set up and send the message
 		$ud       = bp_core_get_core_userdata( $receiver_user_id );
 		$to       = $ud->user_email;
-		$sitename = bp_get_option( 'blogname', 'WordPress' );
-		$subject  = '[' . $sitename . '] ' . sprintf( __( '%s mentioned you in an update', 'buddypress' ), $poster_name );
+		$subject  = bp_get_email_subject( array( 'text' => sprintf( __( '%s mentioned you in an update', 'buddypress' ), $poster_name ) ) );
 
 		if ( bp_is_active( 'groups' ) && bp_is_group() ) {
 			$message = sprintf( __(
@@ -157,12 +156,10 @@ function bp_activity_new_comment_notification( $comment_id, $commenter_id, $para
 		$content = bp_activity_filter_kses( stripslashes($content) );
 
 		// Set up and send the message
-		$ud       = bp_core_get_core_userdata( $original_activity->user_id );
-		$to       = $ud->user_email;
-		$sitename = bp_get_option( 'blogname', 'WordPress' );
-		$subject = '[' . $sitename . '] ' . sprintf( __( '%s replied to one of your updates', 'buddypress' ), $poster_name );
-
-$message = sprintf( __(
+		$ud      = bp_core_get_core_userdata( $original_activity->user_id );
+		$to      = $ud->user_email;
+		$subject = bp_get_email_subject( array( 'text' => sprintf( __( '%s replied to one of your updates', 'buddypress' ), $poster_name ) ) );
+		$message = sprintf( __(
 '%1$s replied to one of your updates:
 
 "%2$s"
@@ -202,8 +199,7 @@ To view your original update and all comments, log in and visit: %3$s
 		// Set up and send the message
 		$ud       = bp_core_get_core_userdata( $parent_comment->user_id );
 		$to       = $ud->user_email;
-		$sitename = bp_get_option( 'blogname', 'WordPress' );
-		$subject = '[' . $sitename . '] ' . sprintf( __( '%s replied to one of your comments', 'buddypress' ), $poster_name );
+		$subject = bp_get_email_subject( array( 'text' => sprintf( __( '%s replied to one of your comments', 'buddypress' ), $poster_name ) ) );
 
 		$poster_name = stripslashes( $poster_name );
 		$content = bp_activity_filter_kses( stripslashes( $content ) );

bp-core/bp-core-filters.php
@@ -255,7 +255,7 @@ function bp_core_activation_signup_blog_notification( $domain, $path, $title, $u
 	$from_name       = bp_get_option( 'blogname', 'WordPress' );
 	$message_headers = "MIME-Version: 1.0\n" . "From: \"{$from_name}\" <{$admin_email}>\n" . "Content-Type: text/plain; charset=\"" . get_option( 'blog_charset' ) . "\"\n";
 	$message         = sprintf( __( "Thanks for registering! To complete the activation of your account and blog, please click the following link:\n\n%1\$s\n\n\n\nAfter you activate, you can visit your blog here:\n\n%2\$s", 'buddypress' ), $activate_url, esc_url( "http://{$domain}{$path}" ) );
-	$subject         = '[' . $from_name . '] ' . sprintf(__( 'Activate %s', 'buddypress' ), esc_url( 'http://' . $domain . $path ) );
+	$subject         = bp_get_email_subject( array( 'text' => sprintf( __( 'Activate %s', 'buddypress' ), 'http://' . $domain . $path ) ) );
 
 	// Send the message
 	$to              = apply_filters( 'bp_core_activation_signup_blog_notification_to',   $user_email, $domain, $path, $title, $user, $user_email, $key, $meta );
@@ -283,7 +283,7 @@ function bp_core_activation_signup_user_notification( $user, $user_email, $key, 
 	$from_name       = bp_get_option( 'blogname', 'WordPress' );
 	$message_headers = "MIME-Version: 1.0\n" . "From: \"{$from_name}\" <{$admin_email}>\n" . "Content-Type: text/plain; charset=\"" . get_option( 'blog_charset' ) . "\"\n";
 	$message         = sprintf( __( "Thanks for registering! To complete the activation of your account please click the following link:\n\n%1\$s\n\n", 'buddypress' ), $activate_url );
-	$subject         = '[' . $from_name . '] ' . __( 'Activate Your Account', 'buddypress' );
+	$subject         = bp_get_email_subject( array( 'text' => __( 'Activate Your Account', 'buddypress' ) ) );
 
 	// Send the message
 	$to      = apply_filters( 'bp_core_activation_signup_user_notification_to',   $user_email, $user, $user_email, $key, $meta );

bp-core/deprecated/1.5.php
@@ -349,8 +349,7 @@ function groups_at_message_notification( $content, $poster_user_id, $group_id, $
 			// Set up and send the message
 			$ud = bp_core_get_core_userdata( $receiver_user_id );
 			$to = $ud->user_email;
-			$sitename = bp_get_option( 'blogname', 'WordPress' );
-			$subject  = '[' . $sitename . '] ' . sprintf( __( '%1$s mentioned you in the group "%2$s"', 'buddypress' ), $poster_name, $group->name );
+			$subject = bp_get_email_subject( array( 'text' => sprintf( __( '%1$s mentioned you in the group "%2$s"', 'buddypress' ), $poster_name, $group->name ) ) );
 
 $message = sprintf( __(
 '%1$s mentioned you in the group "%2$s":

bp-friends/bp-friends-notifications.php
@@ -28,10 +28,8 @@ function friends_notification_new_request( $friendship_id, $initiator_id, $frien
 
 	// Set up and send the message
 	$to       = $ud->user_email;
-	$sitename = bp_get_option( 'blogname', 'WordPress' );
-	$subject  = '[' . $sitename . '] ' . sprintf( __( 'New friendship request from %s', 'buddypress' ), $initiator_name );
-
-	$message = sprintf( __(
+	$subject  = bp_get_email_subject( array( 'text' => sprintf( __( 'New friendship request from %s', 'buddypress' ), $initiator_name ) ) );
+	$message  = sprintf( __(
 '%1$s wants to add you as a friend.
 
 To view all of your pending friendship requests: %2$s
@@ -67,10 +65,8 @@ function friends_notification_accepted_request( $friendship_id, $initiator_id, $
 
 	// Set up and send the message
 	$to       = $ud->user_email;
-	$sitename = bp_get_option( 'blogname', 'WordPress' );
-	$subject  = '[' . $sitename . '] ' . sprintf( __( '%s accepted your friendship request', 'buddypress' ), $friend_name );
-
-	$message = sprintf( __(
+	$subject  = bp_get_email_subject( array( 'text' => sprintf( __( '%s accepted your friendship request', 'buddypress' ), $friend_name ) ) );
+	$message  = sprintf( __(
 '%1$s accepted your friend request.
 
 To view %2$s\'s profile: %3$s

bp-groups/bp-groups-notifications.php
@@ -16,10 +16,9 @@ if ( !defined( 'ABSPATH' ) ) exit;
 function groups_notification_group_updated( $group_id ) {
 
 	$group    = groups_get_group( array( 'group_id' => $group_id ) );
-	$sitename = bp_get_option( 'blogname', 'WordPress' );
-	$subject  = '[' . $sitename . '] ' . __( 'Group Details Updated', 'buddypress' );
-
+	$subject  = bp_get_email_subject( array( 'text' => __( 'Group Details Updated', 'buddypress' ) ) );
 	$user_ids = BP_Groups_Member::get_group_member_ids( $group->id );
+
 	foreach ( (array) $user_ids as $user_id ) {
 		if ( 'no' == bp_get_user_meta( $user_id, 'notification_groups_group_updated', true ) ) continue;
 
@@ -73,8 +72,7 @@ function groups_notification_new_membership_request( $requesting_user_id, $admin
 
 	// Set up and send the message
 	$to       = $ud->user_email;
-	$sitename = bp_get_option( 'blogname', 'WordPress' );
-	$subject  = '[' . $sitename . '] ' . sprintf( __( 'Membership request for group: %s', 'buddypress' ), $group->name );
+	$subject  = bp_get_email_subject( array( 'text' => sprintf( __( 'Membership request for group: %s', 'buddypress' ), $group->name ) ) );
 
 $message = sprintf( __(
 '%1$s wants to join the group "%2$s".
@@ -122,10 +120,9 @@ function groups_notification_membership_request_completed( $requesting_user_id, 
 
 	// Set up and send the message
 	$to       = $ud->user_email;
-	$sitename = bp_get_option( 'blogname', 'WordPress' );
 
 	if ( $accepted ) {
-		$subject = '[' . $sitename . '] ' . sprintf( __( 'Membership request for group "%s" accepted', 'buddypress' ), $group->name );
+		$subject = bp_get_email_subject( array( 'text' => sprintf( __( 'Membership request for group "%s" accepted', 'buddypress' ), $group->name ) ) );
 		$message = sprintf( __(
 'Your membership request for the group "%1$s" has been accepted.
 
@@ -135,7 +132,7 @@ To view the group please login and visit: %2$s
 ', 'buddypress' ), $group->name, $group_link );
 
 	} else {
-		$subject = '[' . $sitename . '] ' . sprintf( __( 'Membership request for group "%s" rejected', 'buddypress' ), $group->name );
+		$subject = bp_get_email_subject( array( 'text' => sprintf( __( 'Membership request for group "%s" rejected', 'buddypress' ), $group->name ) ) );
 		$message = sprintf( __(
 'Your membership request for the group "%1$s" has been rejected.
 
@@ -181,10 +178,8 @@ function groups_notification_promoted_member( $user_id, $group_id ) {
 
 	// Set up and send the message
 	$to       = $ud->user_email;
-	$sitename = bp_get_option( 'blogname', 'WordPress' );
-	$subject  = '[' . $sitename . '] ' . sprintf( __( 'You have been promoted in the group: "%s"', 'buddypress' ), $group->name );
-
-	$message = sprintf( __(
+	$subject  = bp_get_email_subject( array( 'text' => sprintf( __( 'You have been promoted in the group: "%s"', 'buddypress' ), $group->name ) ) );
+	$message  = sprintf( __(
 'You have been promoted to %1$s for the group: "%2$s".
 
 To view the group please visit: %3$s
@@ -231,8 +226,7 @@ function groups_notification_group_invites( &$group, &$member, $inviter_user_id 
 
 		// Set up and send the message
 		$to       = $invited_ud->user_email;
-		$sitename = bp_get_option( 'blogname', 'WordPress' );
-		$subject  = '[' . $sitename . '] ' . sprintf( __( 'You have an invitation to the group: "%s"', 'buddypress' ), $group->name );
+		$subject  = bp_get_email_subject( array( 'text' => sprintf( __( 'You have an invitation to the group: "%s"', 'buddypress' ), $group->name ) ) );
 
 		$message = sprintf( __(
 'One of your friends %1$s has invited you to the group: "%2$s".

bp-members/bp-members-functions.php
@@ -1339,10 +1339,8 @@ function bp_core_signup_send_validation_email( $user_id, $user_email, $key ) {
 	$activate_url = bp_get_activation_page() ."?key=$key";
 	$activate_url = esc_url( $activate_url );
 
-	$from_name = bp_get_option( 'blogname', 'WordPress' );
-
 	$message = sprintf( __( "Thanks for registering! To complete the activation of your account please click the following link:\n\n%1\$s\n\n", 'buddypress' ), $activate_url );
-	$subject = '[' . $from_name . '] ' . __( 'Activate Your Account', 'buddypress' );
+	$subject = bp_get_email_subject( array( 'text' => __( 'Activate Your Account', 'buddypress' ) ) );
 
 	// Send the message
 	$to      = apply_filters( 'bp_core_signup_send_validation_email_to',     $user_email, $user_id                );

bp-messages/bp-messages-notifications.php
@@ -47,8 +47,7 @@ function messages_notification_new_message( $args = array() ) {
 
 			// Set up and send the message
 			$email_to      = $ud->user_email;
-			$sitename      = bp_get_option( 'blogname', 'WordPress' );
-			$email_subject = '[' . $sitename . '] ' . sprintf( __( 'New message from %s', 'buddypress' ), $sender_name );
+			$email_subject = bp_get_email_subject( array( 'text' => sprintf( __( 'New message from %s', 'buddypress' ), $sender_name ) ) );
 
 			$email_content = sprintf( __(
 '%1$s sent you a new message:

commit 1746fbdc7b0a4adae5e0034e6deaa16759935970

Fix variable name typo introduced in r6589 in bp_get_email_subject(). Fixes #4401 again.
git-svn-id: https://buddypress.svn.wordpress.org/trunk@6640 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-core/bp-core-template.php
@@ -558,7 +558,7 @@ function bp_get_email_subject( $args = array() ) {
 		'text'    => ''
 	) );
 
-	$subject = $r['before'] . wp_specialchars_decode( bp_get_option( 'blogname', $default ), ENT_QUOTES ) . $r['after'] . ' ' . $r['text'];
+	$subject = $r['before'] . wp_specialchars_decode( bp_get_option( 'blogname', $r['default'] ), ENT_QUOTES ) . $r['after'] . ' ' . $r['text'];
 
 	return apply_filters( 'bp_get_email_subject', $subject, $r );
 }