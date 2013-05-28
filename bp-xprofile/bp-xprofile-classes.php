buddypress/BP-Default

commit c5413101ce6dd380db2ed30ae39ab6dfcaa95865

Fixes even more WP_DEBUG warnings.
git-svn-id: https://buddypress.svn.wordpress.org/trunk@3369 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-xprofile/bp-xprofile-classes.php
@@ -93,6 +93,7 @@ Class BP_XProfile_Group {
 		$r = wp_parse_args( $args, $defaults );
 		extract( $r, EXTR_SKIP );
 
+		$group_id_sql = '';
 		if ( $profile_group_id )
 			$group_id_sql = $wpdb->prepare( 'WHERE g.id = %d', $profile_group_id );
 
@@ -257,28 +258,28 @@ Class BP_XProfile_Field {
 	function populate( $id, $user_id, $get_data ) {
 		global $wpdb, $userdata, $bp;
 
+		$user_id = 0;
 		if ( is_null( $user_id ) )
 			$user_id = $userdata->ID;
 
 		$sql = $wpdb->prepare( "SELECT * FROM {$bp->profile->table_name_fields} WHERE id = %d", $id );
 
 		if ( $field = $wpdb->get_row( $sql ) ) {
-			$this->id					= $field->id;
-			$this->group_id				= $field->group_id;
-			$this->parent_id			= $field->parent_id;
-			$this->type					= $field->type;
-			$this->name					= stripslashes($field->name);
-			$this->description			= stripslashes($field->description);
-			$this->is_required			= $field->is_required;
-			$this->can_delete			= $field->can_delete;
-			$this->field_order			= $field->field_order;
-			$this->option_order			= $field->option_order;
-			$this->order_by				= $field->order_by;
-			$this->is_default_option	= $field->is_default_option;
+			$this->id               = $field->id;
+			$this->group_id          = $field->group_id;
+			$this->parent_id         = $field->parent_id;
+			$this->type              = $field->type;
+			$this->name              = stripslashes( $field->name );
+			$this->description       = stripslashes( $field->description );
+			$this->is_required       = $field->is_required;
+			$this->can_delete        = $field->can_delete;
+			$this->field_order       = $field->field_order;
+			$this->option_order      = $field->option_order;
+			$this->order_by          = $field->order_by;
+			$this->is_default_option = $field->is_default_option;
 
-			if ( $get_data )
-				$this->data				= $this->get_field_data( $user_id );
-
+			if ( $get_data && $user_id )
+				$this->data            = $this->get_field_data( $user_id );
 		}
 	}
 

bp-activity.php
@@ -32,8 +32,11 @@ function bp_activity_setup_nav() {
 	/* Add 'Activity' to the main navigation */
 	bp_core_new_nav_item( array( 'name' => __( 'Activity', 'buddypress' ), 'slug' => $bp->activity->name, 'position' => 10, 'screen_function' => 'bp_activity_screen_my_activity', 'default_subnav_slug' => 'just-me', 'item_css_id' => $bp->activity->id ) );
 
-	$user_domain = ( !empty( $bp->displayed_user->domain ) ) ? $bp->displayed_user->domain : $bp->loggedin_user->domain;
-	$user_login = ( !empty( $bp->displayed_user->userdata->user_login ) ) ? $bp->displayed_user->userdata->user_login : $bp->loggedin_user->userdata->user_login;
+	if ( !is_user_logged_in() && !isset( $bp->displayed_user->id ) )
+		return;
+
+	$user_domain = ( isset( $bp->displayed_user->domain ) ) ? $bp->displayed_user->domain : $bp->loggedin_user->domain;
+	$user_login = ( isset( $bp->displayed_user->userdata->user_login ) ) ? $bp->displayed_user->userdata->user_login : $bp->loggedin_user->userdata->user_login;
 	$activity_link = $user_domain . $bp->activity->name . '/';
 
 	/* Add the subnav items to the activity nav item if we are using a theme that supports this */
@@ -202,7 +205,7 @@ function bp_activity_screen_notification_settings() {
 			</tr>
 			<tr>
 				<td></td>
-				<td><?php printf( __( "A member replies to an update or comment you've posted", 'buddypress' ), $current_user->user_login ) ?></td>
+				<td><?php _e( "A member replies to an update or comment you've posted", 'buddypress' ) ?></td>
 				<td class="yes"><input type="radio" name="notifications[notification_activity_new_reply]" value="yes" <?php if ( !get_user_meta( $bp->loggedin_user->id, 'notification_activity_new_reply', true ) || 'yes' == get_user_meta( $bp->loggedin_user->id, 'notification_activity_new_reply', true ) ) { ?>checked="checked" <?php } ?>/></td>
 				<td class="no"><input type="radio" name="notifications[notification_activity_new_reply]" value="no" <?php if ( 'no' == get_user_meta( $bp->loggedin_user->id, 'notification_activity_new_reply', true ) ) { ?>checked="checked" <?php } ?>/></td>
 			</tr>
@@ -400,7 +403,7 @@ add_action( 'wp', 'bp_activity_action_remove_favorite', 3 );
 function bp_activity_action_sitewide_feed() {
 	global $bp, $wp_query;
 
-	if ( $bp->current_component != $bp->activity->slug || $bp->current_action != 'feed' || $bp->displayed_user->id || $bp->groups->current_group )
+	if ( $bp->current_component != $bp->activity->slug || $bp->current_action != 'feed' || ( isset( $bp->displayed_user->id ) && $bp->displayed_user->id ) || isset( $bp->groups->current_group ) )
 		return false;
 
 	$wp_query->is_404 = false;
@@ -428,7 +431,7 @@ add_action( 'wp', 'bp_activity_action_personal_feed', 3 );
 function bp_activity_action_friends_feed() {
 	global $bp, $wp_query;
 
-	if ( $bp->current_component != $bp->activity->slug || !$bp->displayed_user->id || $bp->current_action != $bp->friends->slug || $bp->action_variables[0] != 'feed' )
+	if ( $bp->current_component != $bp->activity->slug || !$bp->displayed_user->id || $bp->current_action != $bp->friends->slug || !isset( $bp->action_variables[0] ) || $bp->action_variables[0] != 'feed' )
 		return false;
 
 	$wp_query->is_404 = false;
@@ -442,7 +445,7 @@ add_action( 'wp', 'bp_activity_action_friends_feed', 3 );
 function bp_activity_action_my_groups_feed() {
 	global $bp, $wp_query;
 
-	if ( $bp->current_component != $bp->activity->slug || !$bp->displayed_user->id || $bp->current_action != $bp->groups->slug || $bp->action_variables[0] != 'feed' )
+	if ( $bp->current_component != $bp->activity->slug || !$bp->displayed_user->id || $bp->current_action != $bp->groups->slug || !isset( $bp->action_variables[0] ) || $bp->action_variables[0] != 'feed' )
 		return false;
 
 	$wp_query->is_404 = false;
@@ -456,7 +459,7 @@ add_action( 'wp', 'bp_activity_action_my_groups_feed', 3 );
 function bp_activity_action_mentions_feed() {
 	global $bp, $wp_query;
 
-	if ( $bp->current_component != $bp->activity->slug || !$bp->displayed_user->id || $bp->current_action != 'mentions' || $bp->action_variables[0] != 'feed' )
+	if ( $bp->current_component != $bp->activity->slug || !$bp->displayed_user->id || $bp->current_action != 'mentions' || !isset( $bp->action_variables[0] ) || $bp->action_variables[0] != 'feed' )
 		return false;
 
 	$wp_query->is_404 = false;
@@ -470,7 +473,7 @@ add_action( 'wp', 'bp_activity_action_mentions_feed', 3 );
 function bp_activity_action_favorites_feed() {
 	global $bp, $wp_query;
 
-	if ( $bp->current_component != $bp->activity->slug || !$bp->displayed_user->id || $bp->current_action != 'favorites' || $bp->action_variables[0] != 'feed' )
+	if ( $bp->current_component != $bp->activity->slug || !$bp->displayed_user->id || $bp->current_action != 'favorites' || !isset( $bp->action_variables[0] ) || $bp->action_variables[0] != 'feed' )
 		return false;
 
 	$wp_query->is_404 = false;

bp-activity/bp-activity-classes.php
@@ -12,6 +12,8 @@ Class BP_Activity_Activity {
 	var $content;
 	var $date_recorded;
 	var $hide_sitewide = false;
+	var $mptt_left;
+	var $mptt_right;
 
 	function bp_activity_activity( $id = false ) {
 		global $bp;

bp-activity/bp-activity-templatetags.php
@@ -444,7 +444,7 @@ function bp_activity_avatar( $args = '' ) {
 		$item_id = apply_filters( 'bp_get_activity_avatar_item_id', $activities_template->activity->user_id );
 
 		// If this is a user object pass the users' email address for Gravatar so we don't have to refetch it.
-		if ( 'user' == $object && empty( $email ) )
+		if ( 'user' == $object && empty( $email ) && isset( $activities_template->activity->user_email ) )
 			$email = $activities_template->activity->user_email;
 
 		return apply_filters( 'bp_get_activity_avatar', bp_core_fetch_avatar( array( 'item_id' => $item_id, 'object' => $object, 'type' => $type, 'alt' => $alt, 'class' => $class, 'width' => $width, 'height' => $height, 'email' => $email ) ) );
@@ -655,7 +655,7 @@ function bp_activity_comments( $args = '' ) {
 			if ( !$comment->children )
 				return false;
 
-			$content .= '<ul>';
+			$content = '<ul>';
 			foreach ( (array)$comment->children as $comment ) {
 				if ( !$comment->user_fullname )
 					$comment->user_fullname = $comment->display_name;
@@ -998,15 +998,17 @@ function bp_activities_member_rss_link() {
 	function bp_get_member_activity_feed_link() {
 		global $bp;
 
+		$link = '';
+
 		if ( $bp->current_component == $bp->profile->slug || 'just-me' == $bp->current_action )
 			$link = $bp->displayed_user->domain . $bp->activity->slug . '/feed/';
-		else if ( $bp->friends->slug == $bp->current_action )
+		elseif ( $bp->friends->slug == $bp->current_action )
 			$link = $bp->displayed_user->domain . $bp->activity->slug . '/' . $bp->friends->slug . '/feed/';
-		else if ( $bp->groups->slug == $bp->current_action )
+		elseif ( $bp->groups->slug == $bp->current_action )
 			$link = $bp->displayed_user->domain . $bp->activity->slug . '/' . $bp->groups->slug . '/feed/';
-		else if ( 'favorites' == $bp->current_action )
+		elseif ( 'favorites' == $bp->current_action )
 			$link = $bp->displayed_user->domain . $bp->activity->slug . '/favorites/feed/';
-		else if ( 'mentions' == $bp->current_action )
+		elseif ( 'mentions' == $bp->current_action )
 			$link = $bp->displayed_user->domain . $bp->activity->slug . '/mentions/feed/';
 
 		return apply_filters( 'bp_get_activities_member_rss_link', $link );

bp-core/admin/bp-core-admin.php
@@ -22,6 +22,8 @@ function bp_core_admin_dashboard() { ?>
 
 function bp_core_admin_settings() {
 	global $wpdb, $bp, $current_blog;
+
+	$ud = get_userdata( $bp->loggedin_user->id );
 ?>
 
 	<?php

bp-core/bp-core-classes.php
@@ -237,6 +237,7 @@ class BP_Core_User {
 	function get_users_by_letter( $letter, $limit = null, $page = 1, $populate_extras = true, $exclude = false ) {
 		global $wpdb, $bp;
 
+		$pag_sql = '';
 		if ( $limit && $page )
 			$pag_sql = $wpdb->prepare( " LIMIT %d, %d", intval( ( $page - 1 ) * $limit), intval( $limit ) );
 
@@ -266,6 +267,7 @@ class BP_Core_User {
 		 * Lets fetch some other useful data in a separate queries, this will be faster than querying the data for every user in a list.
 		 * We can't add these to the main query above since only users who have this information will be returned (since the much of the data is in usermeta and won't support any type of directional join)
 		 */
+		$user_ids = array();
 		foreach ( (array)$paged_users as $user )
 			$user_ids[] = $user->id;
 

bp-core/bp-core-filters.php
@@ -126,7 +126,7 @@ function bp_core_login_redirect( $redirect_to ) {
 	if ( is_multisite() && $current_blog->blog_id != BP_ROOT_BLOG )
 		return $redirect_to;
 
-	if ( !empty( $_REQUEST['redirect_to'] ) || strpos( $_REQUEST['redirect_to'], 'wp-admin' ) )
+	if ( isset( $_REQUEST['redirect_to'] ) && ( !empty( $_REQUEST['redirect_to'] ) || strpos( $_REQUEST['redirect_to'], 'wp-admin' ) ) )
 		return $redirect_to;
 
 	if ( false === strpos( wp_get_referer(), 'wp-login.php' ) && false === strpos( wp_get_referer(), 'activate' ) && empty( $_REQUEST['nr'] ) )

bp-core/bp-core-settings.php
@@ -114,10 +114,10 @@ function bp_core_screen_notification_settings() {
 
 	$bp_settings_updated = false;
 
-	if ( $_POST['submit'] ) {
+	if ( isset( $_POST['submit'] ) ) {
 		check_admin_referer('bp_settings_notifications');
 
-		if ( $_POST['notifications'] ) {
+		if ( isset( $_POST['notifications'] ) ) {
 			foreach ( (array)$_POST['notifications'] as $key => $value ) {
 				update_user_meta( (int)$current_user->id, $key, $value );
 			}

commit 4a320f77ecce05dea3f6d645ef7a00783c15e7c7

Don't wipe out the user_id param passed to BP_XProfile_Field::populate()
This fixes a bug introduced in r3369 in an attempt to remove a WP_DEBUG
notice, but inadvertantly prevented plugins from directly calling the
populate() method using the user_id param.

Fixes #4912

git-svn-id: https://buddypress.svn.wordpress.org/trunk@7117 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-xprofile/bp-xprofile-classes.php
@@ -453,10 +453,9 @@ class BP_XProfile_Field {
 	function populate( $id, $user_id, $get_data ) {
 		global $wpdb, $userdata, $bp;
 
-		// @todo Why are we nooping the user_id ?
-		$user_id = 0;
-		if ( is_null( $user_id ) )
+		if ( empty( $user_id ) ) {
 			$user_id = $userdata->ID;
+		}
 
 		$sql = $wpdb->prepare( "SELECT * FROM {$bp->profile->table_name_fields} WHERE id = %d", $id );
 