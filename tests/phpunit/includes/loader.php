1889d1547427cb88e2a4f15829a13ec5a50e7125
Paul Gibbs <paul@byotos.com>
2014-06-10 21:12:45 +0100

    Move BP-Default to the repo root, and remove everything else.
    See https://buddypress.trac.wordpress.org/ticket/5212

--- tests/phpunit/includes/loader.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once( dirname( __FILE__ ) . '/define-constants.php' );
-
-$multisite = (int) ( defined( 'WP_TESTS_MULTISITE') && WP_TESTS_MULTISITE );
-system( WP_PHP_BINARY . ' ' . escapeshellarg( dirname( __FILE__ ) . '/install.php' ) . ' ' . escapeshellarg( WP_TESTS_CONFIG_PATH ) . ' ' . escapeshellarg( WP_TESTS_DIR ) . ' ' . $multisite );
-
-// Bootstrap BP
-require dirname( __FILE__ ) . '/../../../src/bp-loader.php';
